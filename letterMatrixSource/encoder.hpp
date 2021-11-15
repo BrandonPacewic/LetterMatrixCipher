185d8c10f695d3f2cb911e1173fa312e96e6b263
Brandon <pacewicbrandon@gmail.com>
2021-11-15 10:23:15 -0800

    file name update

---
+++ letterMatrixSource/encoder.hpp
@@ -0,0 +1,12 @@
+string encoder(string input, string key) {
+    
+    const string alph = "abcdefghijklmnopqrstuvwxyz";
+    vector<char> matrix_inital; 
+
+    for (auto i: input)
+        matrix_inital.push_back(i);
+
+
+    vector<vector<char>> matrix(5);
+    return input;
+}
