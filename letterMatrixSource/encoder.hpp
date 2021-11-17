8d789f6bd973cf6ca95137ccb74d3ee494d9323a
Brandon <pacewicbrandon@gmail.com>
2021-11-16 18:40:02 -0800

    working save

--- letterMatrixSource/encoder.hpp
+++ letterMatrixSource/encoder.hpp
@@ -1,3 +1,12 @@
+vector<char> used;
+bool find(vector<char> compair) {
+    for (auto i: compair)
+        for (auto j: used)
+            if (i == j)
+                return true;
+    return false;
+}
+
 string encoder(string input, string key) {
     
     const string alph = "abcdefghijklmnopqrstuvwxyz";
@@ -5,6 +14,17 @@ string encoder(string input, string key) {
 
     for (auto i: input)
         matrix_inital.push_back(i);
+
+    for (int i = 0; i < matrix_inital.size();) {
+        if (find(matrix_inital)) {
+            matrix_inital;
+        } else {
+
+            i++;
+        }
+    }
+
+
 
 
     vector<vector<char>> matrix(5);
