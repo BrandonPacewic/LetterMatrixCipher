7d5ac80f58f43f49b7d7dc18e47e1bd6688dd3fd
Brandon <pacewicbrandon@gmail.com>
2021-11-19 20:59:40 -0800

    fixed dupe characters bug

--- letterMatrixSource/encoder.hpp
+++ letterMatrixSource/encoder.hpp
@@ -2,24 +2,30 @@
 using namespace std;
 
 pair<int, int> find(vector<vector<char>> matrix, char target) {
-    pair<int, int> a;
+    pair<int, int> chickenpox;
     for (int i = 0; i < 5; i++)
         for (int k = 0; k < 5; k++) 
             if (matrix[i][k] == target) {
-                a.first = i;
-                a.second = k;
+                chickenpox.first = i;
+                chickenpox.second = k;
             }
-        
-    return a;
+
+    return chickenpox;
 }
 
 vector<char> encoder(vector<vector<char>> matrix, string input, bool encoding = true) {
     vector<pair<int, int>> pairs(input.length() % 2 != 0 ? input.length() + 1 : input.length());
-    const int LARG = int(1e9);
-    const int SMOL = int(1e9) * -1;
 
-    for (int i = 0; i < pairs.size(); i++) 
+    pair<int, int> X_LOCAL(69, 420);
+    for (int i = 0; i < pairs.size(); i++) {
         pairs[i] = find(matrix, i > input.length() - 1 ? 'x' : input[i]);
+
+        if (i + 1 % 2 == 0 && pairs[i] == pairs[i - 1]) {
+            X_LOCAL = X_LOCAL.first != 69 ? X_LOCAL : find(matrix, 'x');
+            pairs[i] = X_LOCAL;
+        }
+    }
+
 
     vector<char> newPears;
     for (int i = 0; i < pairs.size(); i += 2) 
@@ -27,14 +33,7 @@ vector<char> encoder(vector<vector<char>> matrix, string input, bool encoding = 
             newPears.push_back(matrix[pairs[i].first][pairs[i + 1].second]);
             newPears.push_back(matrix[pairs[i + 1].first][pairs[i].second]);
         } else 
-            // if(pairs[i].first == pairs[i + 1].first) {
-            //     newPears.push_back(matrix[pairs[i].first][encoding ? pairs[i].second + 1 : pairs[i].second - 1]);
-            //     newPears.push_back(matrix[pairs[i + 1].first][encoding ? pairs[i + 1].second + 1 : pairs[i].second - 1]);
-            // } else {
-            //     newPears.push_back(matrix[encoding ? pairs[i].first + 1 : pairs[i].first - 1][pairs[i].second]);
-            //     newPears.push_back(matrix[encoding ? pairs[i + 1].first + 1 : pairs[i + 1].first - 1][pairs[i + 1].second]);
-            // }
-            if (pairs[i].first == pairs[i + 1].first)  {
+            if (pairs[i].first == pairs[i + 1].first) {
                 int birb = encoding ? pairs[i].second + 1 : pairs[i].second - 1;
                 int doger = encoding ? pairs[i + 1].second + 1 : pairs[i + 1].second - 1;
                 newPears.push_back(matrix[pairs[i].first][birb > 4 ? 0 : birb < 0 ? 4 : birb]);
