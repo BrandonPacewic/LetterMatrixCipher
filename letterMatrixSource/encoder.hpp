e0d8ef46ebd53b359c82660712e63b9b3143620a
Brandon <pacewicbrandon@gmail.com>
2021-11-19 18:37:45 -0800

    working encoder

--- letterMatrixSource/encoder.hpp
+++ letterMatrixSource/encoder.hpp
@@ -1,25 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//dbg
-#define DBG_MODE
-long long DBG_COUNT = 0ll;
-void DBG_OUT() { cerr << endl; DBG_COUNT += 1ll; }
-template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
-#ifdef DBG_MODE
-template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT += 1ll; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
-#define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
-#else
-template<typename T_Ints> void testList(T_Ints List) { return; }
-#define testArgs(...)
-#endif
-
-template<typename T_Pairs>
-void printPairs(T_Pairs pairs) {
-    for (int i = 0; i < pairs.size(); i++) 
-        cout << "pair# " << i << ": " << pairs[i].first << ' ' << pairs[i].second << '\n';  
-}   
-
 pair<int, int> find(vector<vector<char>> matrix, char target) {
     pair<int, int> a;
     for (int i = 0; i < 5; i++)
@@ -32,31 +13,25 @@ pair<int, int> find(vector<vector<char>> matrix, char target) {
     return a;
 }
 
-auto encoder(vector<vector<char>> matrix, string input, bool encoding = true) {
+vector<char> encoder(vector<vector<char>> matrix, string input, bool encoding = true) {
     vector<pair<int, int>> pairs(input.length() % 2 != 0 ? input.length() + 1 : input.length());
     
     for (int i = 0; i < pairs.size(); i++) 
         pairs[i] = find(matrix, i > input.length() - 1 ? 'x' : input[i]);
-    
-
-    printPairs(pairs);
 
     vector<char> newPears;
-    for (int i = 0; i < pairs.size(); i += 2) {
+    for (int i = 0; i < pairs.size(); i += 2) 
         if (!(pairs[i].first == pairs[i + 1].first || pairs[i].second == pairs[i + 1].second)) {    
             newPears.push_back(matrix[pairs[i].first][pairs[i + 1].second]);
             newPears.push_back(matrix[pairs[i + 1].first][pairs[i].second]);
-        } else {
+        } else 
             if(pairs[i].first == pairs[i + 1].first) {
                 newPears.push_back(matrix[pairs[i].first][encoding ? pairs[i].second + 1 : pairs[i].second - 1]);
-                newPears.push_back(matrix[pairs[i + 1].first][encoding ? pairs[i].second + 1 : pairs[i].second - 1]);
+                newPears.push_back(matrix[pairs[i + 1].first][encoding ? pairs[i + 1].second + 1 : pairs[i].second - 1]);
             } else {
-
+                newPears.push_back(matrix[encoding ? pairs[i].first + 1 : pairs[i].first - 1][pairs[i].second]);
+                newPears.push_back(matrix[encoding ? pairs[i + 1].first + 1 : pairs[i + 1].first - 1][pairs[i + 1].second]);
             }
-        }
-    }
-
-    testList(newPears);    
-
-    return "pass";
+        
+    return newPears;
 }
--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 #include "letterMatrixSource/initializeMatrix.hpp"
 #include "letterMatrixSource/encoder.hpp"
 
-// //dbg
-// #define DBG_MODE
-// long long DBG_COUNT = 0ll;
-// void DBG_OUT() { cerr << endl; DBG_COUNT += 1ll; }
-// template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
-// #ifdef DBG_MODE
-// template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT += 1ll; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
-// #define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
-// #else
-// template<typename T_Ints> void testList(T_Ints List) { return; }
-// #define testArgs(...)
-// #endif
+//dbg
+#define DBG_MODE
+long long DBG_COUNT = 0ll;
+void DBG_OUT() { cerr << endl; DBG_COUNT += 1ll; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { cerr << ' ' << K; DBG_OUT(T...); }
+#ifdef DBG_MODE
+template<typename T_Ints> void testList(T_Ints List) { cerr << '#' << DBG_COUNT << " __LIST_ARGS__: ("; DBG_COUNT += 1ll; for (int i = 0; i < List.size(); i++) { cout << List[i] << (i < List.size() - 1 ? ", " : ")\n"); } }
+#define testArgs(...) cerr << '#' << DBG_COUNT << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", DBG_OUT(__VA_ARGS__)
+#else
+template<typename T_Ints> void testList(T_Ints List) { return; }
+#define testArgs(...)
+#endif
 
 template<typename T_Matrix>
 void printMatrix(T_Matrix MATRIX) {
@@ -29,7 +29,8 @@ int main() {
     cin >> key >> input;
 
     vector<vector<char>> matrix = initalize_matrix(key);
-    cout << encoder(matrix, input) << '\n';
+    vector<char> drag = encoder(matrix, input);
 
     printMatrix(matrix);
-}
+    testList(drag);
+}   
