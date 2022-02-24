8748b4aad2ffcc6ceea722f9977fda4edb162dba
Brandon <pacewicbrandon@gmail.com>
2022-02-24 14:42:43 -0800

    fixed create matrix bug

--- main.cc
+++ main.cc
@@ -12,6 +12,15 @@ typedef std::vector<std::string> matrix;
 
 template<typename T_container, typename T = typename std::enable_if<!std::is_same<T_container, std::string>::value, typename T_container::value_type>::type> std::ostream& operator<<(std::ostream &os, const T_container &v) { os << '{'; std::string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 
+//dbg
+#ifdef DBG_MODE
+int64_t DBG_COUNT = 0;
+void DBG_OUT() { std::cerr << std::endl; DBG_COUNT++; }
+template<typename Front, typename... Back> void DBG_OUT(Front K, Back... T) { std::cerr << ' ' << K; DBG_OUT(T...); }
+#define test(...) std::cerr << '#' << DBG_COUNT << " [" << #__VA_ARGS__ << "]:", DBG_OUT(__VA_ARGS__)
+#else
+#define test(...)
+#endif
 
 matrix create_matrix(const std::string &key) {
 	const int grid_size = 5;
@@ -36,6 +45,7 @@ matrix create_matrix(const std::string &key) {
 		if (used.find(tolower(ch)) != used.end()) { continue; }
 
 		grid[row][cell] = (tolower(ch) == 'j' ? 'i' : tolower(ch));
+		used.insert(tolower(ch) == 'j' ? 'i' : tolower(ch));
 		adjust_row_cell();
 	}
 
@@ -43,6 +53,7 @@ matrix create_matrix(const std::string &key) {
 		if (used.find(ch) != used.end()) { continue; }
 
 		grid[row][cell] = ch;
+		adjust_row_cell();
 	}
 
 	return grid;
