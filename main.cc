d7d909ecfa2567ecb42eccaab5b5160c7a4c60bd
Brandon <pacewicbrandon@gmail.com>
2022-02-25 09:37:36 -0800

    problem with encoding adjustment

--- main.cc
+++ main.cc
@@ -19,14 +19,13 @@ typedef std::vector<std::string> matrix;
 #endif
 	
 void print_matrix(const matrix &grid) {
-    for (int row = 0; row < int(grid.size()); row++) {
-    	for (int cell = 0; cell < int(grid[row].size()); cell++) {
+    for (int row = 0; row < int(grid.size()); ++row) {
+    	for (int cell = 0; cell < int(grid[row].size()); ++cell) {
     		std::cout << grid[row][cell] << (
 					cell >= int(grid[row].size()) - 1 ? '\n' : ' ');
     	}
     }
 }
-
 
 matrix create_matrix(const std::string &key) {
 	const int grid_size = 5;
@@ -38,12 +37,10 @@ matrix create_matrix(const std::string &key) {
 	// null chars left after the matrix is initalized
 	matrix grid(grid_size, std::string(grid_size, '\0'));
 
-	auto adjust_row_cell = [&]() -> void {
-		cell++;
-
+	auto adjust_row_cell = [](int &cell, int &row) -> void {
 		if (!(cell < grid_size)) {
 			cell = 0;
-			row++;
+			++row;
 		}
 	};
 
@@ -52,7 +49,7 @@ matrix create_matrix(const std::string &key) {
 
 		grid[row][cell] = (tolower(ch) == 'j' ? 'i' : tolower(ch));
 		used.insert(tolower(ch) == 'j' ? 'i' : tolower(ch));
-		adjust_row_cell();
+		adjust_row_cell(++cell, row);
 	}
 
 	for (const char &ch : alph) {
@@ -60,12 +57,11 @@ matrix create_matrix(const std::string &key) {
 		if (used.find(ch) != used.end()) { continue; }
 
 		grid[row][cell] = ch;
-		adjust_row_cell();
+		adjust_row_cell(++cell, row);
 	}
 
 	return grid;
 }
-
 
 void assert_valid_chars(const std::string &str) {
 	const std::string valid_chars = "abcdefghijklmnopqrstuvwxyz";
@@ -75,14 +71,14 @@ void assert_valid_chars(const std::string &str) {
 	}
 }
 
-void encoder(const matrix &grid, const bool &encoding, 
+std::string encoder(const matrix &grid, const bool &encoding, 
 		std::string message) {
 	const int matrix_size = int(grid.size());
 	std::unordered_map<char, std::pair<int, int>> map_char_to_cord;
 
 	// Create map char to cord
-	for (int row = 0; row < matrix_size; row++) {
-		for (int cell = 0; cell < matrix_size; cell++) {
+	for (int row = 0; row < matrix_size; ++row) {
+		for (int cell = 0; cell < matrix_size; ++cell) {
 			map_char_to_cord[grid[row][cell]] = {row, cell};
 		}
 	}
@@ -96,24 +92,29 @@ void encoder(const matrix &grid, const bool &encoding,
 	assert(message.length() % 2 == 0);
 
 	// If any pair of chars is the same, the second one must be converted
-	// to 'x'; defined in README.md
+	// to 'x'; defined in README.md. Also at this step every 'j' must 
+	// be replaced with in 'i'; defined in README.md
 	for (int i = 0; i < int(message.length()); i += 2) {
+		if (message[i] == 'j') { message[i] = 'j'; }
+		if (message[i+1] == 'j') { message[i+1] = 'j'; }
 		if (message[i] == message[i+1]) { message[i+1] = 'x'; }
 	}
 
 	// Create message cord pairs
-	std::vector<std::pair<int, int>> message_cord_pairs;
-	for (const char &ch : message) {
-		message_cord_pairs.push_back(map_char_to_cord[ch]);
+	std::vector<std::pair<int, int>> message_cord_pairs(int(message.length()));
+	for (int i = 0; i < int(message.length()); ++i) {
+		message_cord_pairs[i] = map_char_to_cord[message[i]];
 	}
 
 	// Adjusted modulo, creates a number between 0 and b insted
 	// of being between -b and b, this is an oddity found in both C
 	// and c++
 	// http://www.cplusplus.com/forum/general/19502/
-	auto mod = [](int a, int b) -> int {
+	auto mod = [](int a) -> int {
+		const int b = 4; // Valid index in matrix
 		a %= b;
 		if (a < 0) a += b;
+		assert(a >= 0 && a <= b);
 		return a; 
 	};
 
@@ -122,15 +123,41 @@ void encoder(const matrix &grid, const bool &encoding,
 	const int ad = (encoding ? 1 : -1);
 	std::vector<std::pair<int, int>> new_cord_pairs;
 
-	for (int i = 0; i < int(message_cord_pairs.size()); i += 2) {
-		if (message_cord_pairs[i].first == message_cord_pairs[i+1].first) {
-			new_cord_pairs.push_back({
-				// working
-			});
+	// Make new char pairs
+	for (int i = 0; i < int(message_cord_pairs.size()); i += 2) 
+	{
+		if (message_cord_pairs[i].first == message_cord_pairs[i+1].first) 
+		{
+			new_cord_pairs.push_back({message_cord_pairs[i].first, 
+				mod(message_cord_pairs[i].second + ad)});
+			new_cord_pairs.push_back({message_cord_pairs[i+1].first, 
+				mod(message_cord_pairs[i+1].second + ad)});
+		}
+		else if (message_cord_pairs[i].second == message_cord_pairs[i+1].second) 
+		{
+			new_cord_pairs.push_back({mod(message_cord_pairs[i].first + ad),
+				message_cord_pairs[i].second});
+			new_cord_pairs.push_back({mod(message_cord_pairs[i+1].first + ad),
+				message_cord_pairs[i+1].second});
+		}
+		else
+		{
+			new_cord_pairs.push_back({message_cord_pairs[i].first, 
+				message_cord_pairs[i+1].second});
+			new_cord_pairs.push_back({message_cord_pairs[i+1].first,
+				message_cord_pairs[i].second});
 		}
 	}
-}
 
+	std::string new_message = "";
+
+	// Create new message
+	for (const std::pair<int, int> &cord : new_cord_pairs) {
+		new_message += grid[cord.first][cord.second];
+	}
+
+	return new_message;
+}
 
 int main() {
 #ifdef DBG_MODE
@@ -141,18 +168,19 @@ int main() {
 #endif
 
 	std::string message, key;
-	std::cout << "(Message, Key): " << std::flush;
+	// std::cout << "(Message, Key): " << std::flush;
 	std::cin >> message >> key;
 
 	auto grid = create_matrix(key);
 	print_matrix(grid);
 
 	bool encoding;
-	std::cout << "Encoding or Decoding(1, 0): " << std::flush;
+	// std::cout << "Encoding or Decoding(1, 0): " << std::flush;
 	std::cin >> encoding;
 	test(encoding);
 
-	encoder(grid, encoding, message);
+	std::string new_message = encoder(grid, encoding, message);
+	std::cout << new_message << std::endl;
 
 	return 0;
 }
