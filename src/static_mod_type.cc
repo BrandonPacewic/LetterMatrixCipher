e571792363dd3ea3670ce4af1aa266fbde87066c
Brandon <pacewicbrandon@gmail.com>
2022-03-07 14:54:39 -0800

    changed mod_type to static_mod_type

---
+++ src/static_mod_type.cc
@@ -0,0 +1,153 @@
+/*
+ * 2022 - static_mod_type class
+ * Brandon Pacewic
+*/
+
+#pragma once
+#ifndef _BRANDON_STATIC_MOD_TYPE_C
+#define _BRANDON_STATIC_MOD_TYPE_C
+
+#include <cstdint>
+#include <iostream>
+#include <istream>
+
+#include "static_mod_type.h"
+using bp::static_mod_type;
+using bp::ceil_num;
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::static_mod_type() : value{0} {}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::static_mod_type(_Tp _value) : value{_value} {}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::operator int() const { return value; }
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::operator int64_t() const { return value; }
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::operator float() const { return value; }
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::operator double() const { return value; }
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>::operator long double() const { return value; }
+
+// Side feature of static_mod_type<> class, modular arithmetic in C++ works
+// different than it otherwise should. In C and C++ the % operator creates a
+// value between -b and b when in most other cases the expected return value
+// should be between 0 and b. This is a simple fix. This, not the main feature
+// of this class, is found in both the static_mod_type<> class as well as the
+// mod_type<> class. This change also can be found for the operator%
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>& static_mod_type<_Tp, Ceil>::operator%=(
+        const static_mod_type<_Tp, Ceil>& other) {
+    value %= other.value;
+    if (value < 0) { value += other.value; }
+    return *this;
+}
+
+template<typename _Tp, const ceil_num Ceil> 
+static_mod_type<_Tp, Ceil>& static_mod_type<_Tp, Ceil>::operator+=(
+        const static_mod_type<_Tp, Ceil>& other) {
+    value += other.value;
+    return *this %= Ceil;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>& static_mod_type<_Tp, Ceil>::operator-=(
+        const static_mod_type<_Tp, Ceil>& other) {
+    value -= other.value;
+    return *this %= Ceil;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>& static_mod_type<_Tp, Ceil>::operator*=(
+        const static_mod_type<_Tp, Ceil>& other) {
+    value *= other.value;
+    return *this %= Ceil;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil>&
+static_mod_type<_Tp, Ceil>::operator/=(const static_mod_type<_Tp, Ceil>& other) 
+{ return value /= other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil> static_mod_type<_Tp, Ceil>::operator+(
+        const static_mod_type<_Tp, Ceil>& other) const { 
+    auto tmp = *this;
+    return tmp += other;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil> static_mod_type<_Tp, Ceil>::operator-(
+        const static_mod_type<_Tp, Ceil>& other) const {
+    auto tmp = *this;
+    return tmp -= other;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil> static_mod_type<_Tp, Ceil>::operator*(
+        const static_mod_type<_Tp, Ceil>& other) const {
+    auto tmp = *this;
+    return tmp *= other;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil> static_mod_type<_Tp, Ceil>::operator/(
+        const static_mod_type<_Tp, Ceil>& other) const {
+    auto tmp = *this;
+    return tmp /= other;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+static_mod_type<_Tp, Ceil> static_mod_type<_Tp, Ceil>::operator%(
+        const static_mod_type<_Tp, Ceil>& other) const {
+    auto tmp = *this;
+    return tmp %= other;
+}
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator==(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value == other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator!=(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value != other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator<(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value < other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator>(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value > other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator<=(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value <= other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+bool static_mod_type<_Tp, Ceil>::operator>=(
+        const static_mod_type<_Tp, Ceil>& other) const
+{ return value >= other.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+std::ostream& operator<<(std::ostream& os, const static_mod_type<_Tp, Ceil>& a)
+{ return os << a.value; }
+
+template<typename _Tp, const ceil_num Ceil>
+std::istream& operator>>(std::istream& is, static_mod_type<_Tp, Ceil>& a)
+{ return is >> a.value; }
+
+#endif // _BRANDON_STATIC_MOD_TYPE_C
---
+++ src/static_mod_type.h
@@ -0,0 +1,53 @@
+/*
+ * static mod type
+ * Brandon Pacewic
+ */
+
+#pragma once
+#ifndef _BRANDON_STATIC_MOD_TYPE_H
+#define _BRANDON_STATIC_MOD_TYPE_H
+
+#include <cstdint>
+
+namespace bp {
+
+// TODO: Check overflow; default is int
+typedef int ceil_num;
+
+template<typename _Tp, const int Ceil>
+class static_mod_type {
+public:
+	_Tp value;
+
+	static_mod_type();
+	static_mod_type(_Tp);
+
+	explicit operator int() const;
+	explicit operator int64_t() const;
+	explicit operator float() const;
+	explicit operator double() const;
+	explicit operator long double() const;
+
+	static_mod_type& operator+=(const static_mod_type&);
+	static_mod_type& operator-=(const static_mod_type&);
+	static_mod_type& operator*=(const static_mod_type&);
+	static_mod_type& operator/=(const static_mod_type&);
+	static_mod_type& operator%=(const static_mod_type&);
+
+	static_mod_type operator+(const static_mod_type&) const;
+	static_mod_type operator-(const static_mod_type&) const;
+	static_mod_type operator*(const static_mod_type&) const;
+	static_mod_type operator/(const static_mod_type&) const;
+	static_mod_type operator%(const static_mod_type&) const;
+
+	bool operator==(const static_mod_type&) const;
+	bool operator!=(const static_mod_type&) const;
+	bool operator<(const static_mod_type&) const;
+	bool operator>(const static_mod_type&) const;
+	bool operator<=(const static_mod_type&) const;
+	bool operator>=(const static_mod_type&) const;
+};
+
+} // namespace bp
+
+#endif
